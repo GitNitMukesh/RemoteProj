294ef7a0eec7d982a6c29d8bf9f3de1a87f2add0
GitNitMukesh <mukeshsatapathy@gmail.com>
2018-04-06 18:22:20 +0530

    initial commit

---
+++ SpringBootWebProj/src/main/java/org/springboot/mvc/SpringBootWebProjApplication.java
@@ -0,0 +1,20 @@
+package org.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+@ComponentScan(basePackages="org.springboot.mvc.controller")
+public class SpringBootWebProjApplication {
+
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebProjApplication.class, args);
+	}
+	
+	
+	
+}
---
+++ SpringBootWebProj/src/main/java/org/springboot/mvc/controller/HomeController.java
@@ -0,0 +1,21 @@
+package org.springboot.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/")
+	public String getWishMessage() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/home.htm", method = RequestMethod.GET)
+	public String getMessage() {
+		return "home";
+	}
+
+}
